1d79bb20fde46f4ba8c0ecf6cd4028274c9dbb16
Noah Hildebrand <nhilde21@gmail.com>
2022-03-22 00:16:15 -0400

    worked on adding styles and setting up questions

---
+++ assets/script.js
@@ -0,0 +1,31 @@
+const startBtn = document.querySelector("#startBtn");
+const startCard = document.querySelector("#startCard");
+const question1 = document.querySelector("#question1");
+const question2 = document.querySelector("#question2");
+const question3 = document.querySelector("#question3");
+const question4 = document.querySelector("#question4");
+const question5 = document.querySelector("#question5");
+//example timer setup below
+//const timeIs = theQuestions.length * 18
+
+
+function start() {
+// hide the start area
+
+startCard.setAttribute("class", "hide")
+question1.setAttribute("class", "show")
+
+
+// unhide the questions 
+
+
+// start the timer
+
+// beign the question logic 
+
+
+}
+
+
+// add the onclick function for the start button here 
+startBtn.onclick = start;
