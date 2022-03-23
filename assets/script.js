2a6f51bd4ec3ee1d0419f9764af1013461e4f154
Noah Hildebrand <nhilde21@gmail.com>
2022-03-23 01:05:40 -0400

    worked on timer and css elements

--- assets/script.js
+++ assets/script.js
@@ -5,8 +5,29 @@ const question2 = document.querySelector("#question2");
 const question3 = document.querySelector("#question3");
 const question4 = document.querySelector("#question4");
 const question5 = document.querySelector("#question5");
+const timer = document.querySelector("#timer");
+const startTime = 60;
+
 //example timer setup below
 //const timeIs = theQuestions.length * 18
+
+function setTime() {
+var timerInterval = setInterval(function() {
+startTime--;
+timer.textContent = startTime + "Seconds left" ; 
+
+if (timer === 0) {
+    clearInterval(timerInterval);
+    $(".questions").hide();
+    alert("Time is up");
+}
+}, 60000);
+
+
+}
+
+
+$("#startBtn").on("click", setTime)
 
 console.log("test")
 $("#startBtn").on("click", function() {
